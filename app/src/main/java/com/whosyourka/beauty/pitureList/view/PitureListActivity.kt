7f448aa1efd7f398545eff878ab26a536502683b
huangmingcan <huangmingcan@lakala.com>
2019-02-14 14:08:34 +0800

    图片列表页面初始化

---
+++ app/src/main/java/com/whosyourka/beauty/pitureList/view/PitureListActivity.kt
@@ -0,0 +1,13 @@
+package com.whosyourka.beauty.pitureList.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.whosyourka.beauty.R
+
+class PitureListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_piture_list)
+    }
+}
