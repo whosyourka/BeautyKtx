dc9a455b4078946ff602f8e82cfa12489f6f20cb
whosyourka <whosyourka@gmail.com>
2019-04-29 16:29:04 +0800

    比较2个动画效率

--- app/src/main/java/com/whosyourka/beauty/MainActivity.kt
+++ app/src/main/java/com/whosyourka/beauty/MainActivity.kt
@@ -1,21 +1,39 @@
 package com.whosyourka.beauty
 
+import android.animation.ValueAnimator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.TranslateAnimation
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import com.whosyourka.beauty.pitureList.view.PitureListActivity
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var btnTest: Button
+    private lateinit var btnMsg: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.tv_instrument_test).setOnClickListener {
-            findViewById<TextView>(R.id.tv_msg).text = "instrument_test"
-            startActivity(Intent( this,PitureListActivity::class.java))
+        btnMsg = findViewById<Button>(R.id.tv_msg)
+        btnMsg.setOnClickListener {
+            TranslateAnimation(0F,100F,0F,0F).apply {
+                duration = 1000
+                btnMsg.startAnimation(this)
+            }
         }
+        btnTest = findViewById<Button>(R.id.tv_instrument_test)
+        btnTest.setOnClickListener {
+            ValueAnimator.ofInt(0, 100).apply {
+                duration = 1000
+                addUpdateListener {
+                    btnTest.translationX= (it.animatedValue as Int).toFloat()
+                }
+                start()
+            }
+        }
+
     }
 }
--- app/src/test/java/com/whosyourka/beauty/Banana.kt
+++ app/src/test/java/com/whosyourka/beauty/Banana.kt
@@ -3,8 +3,21 @@ package com.whosyourka.beauty
 /**
  * Created by HMC on 2019/3/27
  */
-object Banana {
-    fun getColor():String{
-        return "红色"
+class Banana {
+    fun getColor() {
+        val b: Int? = 22
+        val a: Int? = b
+        if (a == b) {
+            println("21" + a)
+        }
     }
+
+    lateinit var subject: Banana
+
+//    companion object {
+//        @JvmStatic
+//        fun getColor():String{
+//            return "红色"
+//        }
+//    }
 }
