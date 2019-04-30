5c5671e49cf97789dc0ac20e151b83ed89a9e48c
whosyourka <whosyourka@gmail.com>
2019-04-30 11:34:03 +0800

    添加leakcarry

---
+++ app/src/main/java/com/whosyourka/beauty/App.java
@@ -0,0 +1,14 @@
+package com.whosyourka.beauty;
+
+import android.app.Application;
+
+/**
+ * Created by HMC on 2019/4/29
+ **/
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
