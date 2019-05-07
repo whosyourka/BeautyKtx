cc10b1c30695a8df9eb599708e4365d51d8b998f
whosyourka <whosyourka@gmail.com>
2019-05-07 15:57:08 +0800

    WorkManager修改

---
+++ app/src/main/java/com/whosyourka/beauty/archit/CommonWorker.kt
@@ -0,0 +1,33 @@
+package com.whosyourka.beauty.archit
+
+import android.content.Context
+import androidx.work.Worker
+import androidx.work.WorkerParameters
+
+/**
+ * Created by HMC on 2019/5/7
+ **/
+class CommonWorker(appContext: Context, workerParams: WorkerParameters)
+    : Worker(appContext, workerParams) {
+    var retry = false;
+    override fun doWork(): Result {
+        println("id：" + id)
+        var number = this.inputData.getInt("key_accept_bg_work", 0)
+        if (0 == number) {
+            return Result.failure()
+        } else if (1 == number) {
+            println("retry:" + retry)
+            if (retry) {
+                return Result.success()
+            }
+            retry = true
+            Thread.sleep(5000)
+            return Result.retry()
+        }
+        //...set the output, and we're done!
+//            val output: Data = mapOf("key_accept_bg_work" to 2).toWorkData()
+//            setOutputData(output)
+        return Result.success()
+    }
+
+}
--- app/src/main/java/com/whosyourka/beauty/archit/WorkManagerInstance.kt
+++ app/src/main/java/com/whosyourka/beauty/archit/WorkManagerInstance.kt
@@ -62,30 +62,7 @@ class WorkManagerInstance private constructor() {
     }
     */
 
-    class CommonWorker(appContext: Context, workerParams: WorkerParameters)
-        : Worker(appContext, workerParams) {
-        var retry = false;
-        override fun doWork(): Result {
-            println("id：" + id)
-            var number = this.inputData.getInt("key_accept_bg_work", 0)
-            if (0 == number) {
-                return Result.failure()
-            } else if (1 == number) {
-                println("retry:" + retry)
-                if (retry) {
-                    return Result.success()
-                }
-                retry = true
-                Thread.sleep(5000)
-                return Result.retry()
-            }
-            //...set the output, and we're done!
-//            val output: Data = mapOf("key_accept_bg_work" to 2).toWorkData()
-//            setOutputData(output)
-            return Result.success()
-        }
 
-    }
 
 
 }
