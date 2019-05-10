b706c70a5c67f2b4406dc27e2895d3200dbccf3b
whosyourka <whosyourka@gmail.com>
2019-05-10 15:27:13 +0800

    去掉databinding

--- app/src/main/java/com/whosyourka/beauty/pitureList/view/PitureListActivity.kt
+++ app/src/main/java/com/whosyourka/beauty/pitureList/view/PitureListActivity.kt
@@ -2,10 +2,10 @@ package com.whosyourka.beauty.pitureList.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
+//import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProviders
 import com.whosyourka.beauty.R
-import com.whosyourka.beauty.databinding.ActivityPitureListBinding
+//import com.whosyourka.beauty.databinding.ActivityPitureListBinding
 import com.whosyourka.beauty.pitureList.model.PitureListModle
 import com.whosyourka.beauty.pitureList.vm.PitureListVM
 
@@ -17,13 +17,13 @@ class PitureListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_piture_list)
 
-        val binding: ActivityPitureListBinding = DataBindingUtil.setContentView(
-            this, R.layout.activity_piture_list)
-        var model = PitureListModle()
-        model.name = "whosyourka"
-        binding.viewmodel = model
-
-        viewModle.showNoData(model)
+//        val binding: ActivityPitureListBinding = DataBindingUtil.setContentView(
+//            this, R.layout.activity_piture_list)
+//        var model = PitureListModle()
+//        model.name = "whosyourka"
+//        binding.viewmodel = model
+//
+//        viewModle.showNoData(model)
     }
 }
 
